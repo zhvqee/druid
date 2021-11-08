dd58fc6f626361d6d59622e6f700a5853e5a735f
zhuqi <281961682@qq.com>
2021-11-08 22:30:05 +0800

    pom

--- druid-demo/src/main/java/com/druid/demo/controller/DruidTestController.java
+++ druid-demo/src/main/java/com/druid/demo/controller/DruidTestController.java
@@ -1,10 +1,16 @@
 package com.druid.demo.controller;
 
+import com.druid.demo.mapper.TbOrderMapper;
+import com.druid.demo.model.TbOrder;
 import com.google.common.collect.Maps;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
 
 /**
  * @ProjectName: druid
@@ -18,16 +24,45 @@ import java.util.Map;
 @RequestMapping("/druid/demo")
 public class DruidTestController {
 
+    @Autowired
+    private TbOrderMapper tbOrderMapper;
+
     @RequestMapping("/insert")
     public Map<String, Object> insertTest() {
         Map<String, Object> result = Maps.newHashMap();
+        tbOrderMapper.insert(buildTbOrder());
         return result;
     }
 
     @RequestMapping("/listTest")
-    public Map<String, Object> listTest() {
+    public Map<String, Object> listTest(Long id) {
         Map<String, Object> result = Maps.newHashMap();
-        result.put("data", "123");
+        TbOrder tbOrder = tbOrderMapper.selectByPrimaryKey(id);
+        result.put("data", tbOrder);
         return result;
+    }
+
+    private TbOrder buildTbOrder() {
+        Random random = new Random();
+        TbOrder tbOrder = new TbOrder();
+        //tbOrder.setOrderNo(UUID.randomUUID().toString());
+        tbOrder.setOrderNo(UUID.randomUUID().toString());
+        tbOrder.setUserId(Long.valueOf(UUID.randomUUID().toString().hashCode()));
+        tbOrder.setShopId(random.nextLong());
+
+        tbOrder.setPaymentChannel(1);
+        tbOrder.setOrderStatus(2);
+        tbOrder.setCreateTime(new Date());
+        tbOrder.setPaymentTime(new Date());
+        tbOrder.setOrderSn(UUID.randomUUID().toString());
+        tbOrder.setGoodsCount(1);
+        tbOrder.setGoodsAmount(100L);
+        tbOrder.setActualPayAmount(100L);
+
+        tbOrder.setLogisticsFee(0L);
+        tbOrder.setAddressId(random.nextLong());
+        tbOrder.setDeliveryTime(new Date());
+        tbOrder.setCompletedTime(new Date());
+        return tbOrder;
     }
 }
